2a9d266641f1c4320e4fda73582ddce2fa1731b3
pyroscorcher <158665745+pyroscorcher@users.noreply.github.com>
2024-05-16 14:00:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-top').classList.add('active');
+    }
+    else{
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out')
+}
+
+function fadeOut(){
+    setInterval(loader, 750);
+}
+
+window.onload = fadeOut();
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup-container").style.
+            display = "block";
+        },
+        2000
+    )
+});
+
+document.querySelector(".close").addEventListener
+("click", function(){
+    document.querySelector(".popup-container").style.display = 
+    "none";
+});
